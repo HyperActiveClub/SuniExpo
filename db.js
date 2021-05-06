b53cd03c966625cc04bc3e1540be07dae979a6b6
Muhamad Kadafi <muhkadafi08@gmail.com>
2021-05-06 13:16:30 +0700

    Delete db.js

--- db.js
+++
@@ -1,6 +0,0 @@
-var firebase = require("firebase");
-var Config=require('./config.js');
-require("firebase/firestore");
-var firebaseApp=firebase.initializeApp(Config.firebase);
-exports.fb=firebaseApp;
-exports.fbs=firebaseApp.firestore();
